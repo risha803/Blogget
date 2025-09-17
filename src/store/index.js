3cb264060b01c8b5230ad4844c25c8bcd7bbe8ec
Risha Knyazeva <rknyazeva11@gmail.com>
2025-09-17 13:33:00 +0300

    edit useToken

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,13 @@
 import {createStore} from 'redux';
+import {getToken, setToken} from '../hooks/token';
 
 const UPDATE_COMMENT = 'UPDATE_COMMENT';
+const UPDATE_TOKEN = 'UPDATE_TOKEN';
+const DELETE_TOKEN = 'DELETE_TOKEN';
+
 const initialState = {
   comment: 'Привет Redux',
+  token: getToken(),
 };
 
 export const updateComment = comment => ({
@@ -16,6 +21,18 @@ const rootReducer = (state = initialState, action) => {
       return {
         ...state,
         comment: action.comment,
+      };
+      case UPDATE_TOKEN:
+        setToken(action.token);
+      return {
+        ...state,
+        token: action.token,
+      };
+      case DELETE_TOKEN:
+        setToken('');
+      return {
+        ...state,
+        token: '',
       };
     default:
       return state;
