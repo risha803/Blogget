c6efc328485d8d2bc4e2530ed9d3189c2dbd1158
Risha Knyazeva <rknyazeva11@gmail.com>
2025-09-19 13:10:41 +0300

    edit eslint

--- src/components/Main/List/Post/PostDate/PostDate.jsx
+++ src/components/Main/List/Post/PostDate/PostDate.jsx
@@ -2,11 +2,9 @@ import style from './PostDate.module.css';
 import PropTypes from 'prop-types';
 import formatDate from '../../../../../utilits/formatDate';
 
-export const PostDate = ({date}) => {
-  return (
-    <time className={style.date} dateTime={date}>{formatDate(date)}</time>
-  );
-};
+export const PostDate = ({date}) => (
+  <time className={style.date} dateTime={date}>{formatDate(date)}</time>
+);
 
 PostDate.propTypes = {
   date: PropTypes.string.isRequired,
--- src/components/Main/List/Post/PostRaiting/PostRaiting.jsx
+++ src/components/Main/List/Post/PostRaiting/PostRaiting.jsx
@@ -2,16 +2,14 @@ import style from './PostRaiting.module.css';
 import PropTypes from 'prop-types';
 import {Text} from '../../../../../UI/Text/Text';
 
-export const PostRaiting = ({ups}) => {
-  return (
-    <div className={style.rating}>
-      <button className={style.up} aria-label='Увеличивать рейтинг' />
-      <Text As='p' bold size={12} tsize={16}
-        color='#8f8f8f' className={style.ups}>{ups}</Text>
-      <button className={style.down} aria-label='Уменьшить рейтинг' />
-    </div>
-  );
-};
+export const PostRaiting = ({ups}) => (
+  <div className={style.rating}>
+    <button className={style.up} aria-label='Увеличивать рейтинг' />
+    <Text As='p' bold size={12} tsize={16}
+      color='#8f8f8f' className={style.ups}>{ups}</Text>
+    <button className={style.down} aria-label='Уменьшить рейтинг' />
+  </div>
+);
 
 PostRaiting.propTypes = {
   ups: PropTypes.number.isRequired,
